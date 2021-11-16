3b5d4384091a8bcdabf0f99259e5b687cbc833a3
lijiayi123456 <1057339032@qq.com>
2021-11-16 16:06:59 +0800

    项目引入elementelement-ui + scss
    Signed-off-by: lijiayi123456 <1057339032@qq.com>

--- common-code-project/src/App.vue
+++ common-code-project/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <p>李佳艺的主页</p>
+    <el-button>测试element-ui按钮</el-button>
   </div>
-  <router-view/>
+  
 </template>
 
 <style lang="scss">
@@ -14,17 +14,9 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+div {
+  p {
+    color: blue;
   }
 }
 </style>
