637c506f7dddf8d6e1627b91693991cd14e2c270
lijiayi123456 <1057339032@qq.com>
2021-11-16 16:13:23 +0800

    修改publicPath，打包，提交dist
    Signed-off-by: lijiayi123456 <1057339032@qq.com>

---
+++ common-code-project/dist/precache-manifest.d2dd8e4d34049b3d8a887605006ccffb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f407c599aea4e33ba6",
+    "url": "css/app.f2ba2701.css"
+  },
+  {
+    "revision": "7da18e6506f64423a4e8",
+    "url": "css/chunk-vendors.e8d1e5d4.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6feb3ba8ed9c7cf577e17ce9d978f7d2",
+    "url": "index.html"
+  },
+  {
+    "revision": "e4f407c599aea4e33ba6",
+    "url": "js/app.c493b923.js"
+  },
+  {
+    "revision": "7da18e6506f64423a4e8",
+    "url": "js/chunk-vendors.3f85a468.js"
+  },
+  {
+    "revision": "8daf5cf5b159a0ebb45a5321e458e404",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
